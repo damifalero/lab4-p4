7a637142985b0364449e91567971efc16e0b3a26
Yennifer Gutiérrez <93296508+yennifergutierrez@users.noreply.github.com>
2026-06-02 12:20:40 -0300

    Create ManejadorVehiculo.cpp

---
+++ src/ManejadorVehiculo.cpp
@@ -0,0 +1,19 @@
+#include "../include/ManejadorVehiculo.h"
+
+ManejadorVehiculo::ManejadorVehiculo(){
+
+}
+
+ManejadorVehiculo ManejadorVehiculo::getInstancia(){
+
+}
+
+void ManejadorVehiculo::agregarVehiculo(Vehiculo v){
+
+}
+bool ManejadorVehiculo::existeVehiculo(std::string matricula){
+
+}
+Vehiculo ManejadorVehiculo::obtenerVehiculo(std::string matricula){
+    
+}
