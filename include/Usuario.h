c9dbe820ffd2a6c8a1a04fbc24a4498e5d62b508
Agustin Lara <agustin.lara@fing.edu.uy>
2026-06-02 20:52:26 -0300

    dtfecha, dtvehiculosconductor y usuario

--- include/Usuario.h
+++ include/Usuario.h
@@ -1,7 +1,10 @@
 #ifndef USUARIO_H
 #define USUARIO_H
 
+#include "../include/DTUsuario.h"
+#include "../include/Calificacion.h"
 #include <string>
+#include <set>
 
 class Usuario {
 protected:
@@ -9,10 +12,25 @@ protected:
     std::string nombre;
     std::string contrasena;
     std::string email;
+    float caliPromedio;
+    std::set<Calificacion> calificaciones;
 
 public:
     Usuario(std::string nickname, std::string nombre, std::string contrasena, std::string email);
     virtual ~Usuario();
+
+    //Getters
+    std::string getNickname();
+    std::string getNombre();
+    std::string getContrasenia();
+    std::string getEmail();
+    float getPromedio();
+
+    //Funciones
+    bool esPasajero();
+    DTUsuario getDTUsuario();
+    void asociarCalificacion(Calificacion);
+    void actualizarPromedio();
 };
 
 #endif
--- src/DTVehiculosConductor.cpp
+++ src/DTVehiculosConductor.cpp
@@ -1,9 +1,18 @@
 #include "../include/DTVehiculosConductor.h"
 
-DTVehiculosConductor::DTVehiculosConductor(std::vector<DTDetalleVehiculo> vehiculos)
-    {
-    this->vehiculos = vehiculos;
+DTVehiculosConductor::DTVehiculosConductor(std::string matricula, std::string modelo, int capacidad) {
+    this->matricula = matricula;
+    this->modelo = modelo;
+    this->capacidad = capacidad;
 }
+
+void DTVehiculosConductor::setMatricula(std::string matricula) { this->matricula = matricula; }
+void DTVehiculosConductor::setModelo(std::string modelo) { this->modelo = modelo; }
+void DTVehiculosConductor::setCapacidad(int capacidad) { this->capacidad = capacidad; }
+
+std::string DTVehiculosConductor::getMatricula(){ return this->matricula; }
+std::string DTVehiculosConductor::getModelo(){ return this->modelo; }
+int DTVehiculosConductor::getCapacidad(){ return this->capacidad; }
 
 std::vector<DTDetalleVehiculo> DTVehiculosConductor::getVehiculos() {
     return vehiculos;
