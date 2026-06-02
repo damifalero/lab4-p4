13c6e04771e59dd84d6fe6526dceaebf7a97e53c
Yennifer Gutiérrez <93296508+yennifergutierrez@users.noreply.github.com>
2026-06-02 12:19:57 -0300

    Update Viaje.cpp

--- src/Viaje.cpp
+++ src/Viaje.cpp
@@ -10,3 +10,41 @@ Viaje::Viaje(int codigo, DTFecha fecha, std::string origen, std::string destino,
 }
 
 Viaje::~Viaje() {}
+
+int Viaje::getCodigo(){ return codigo; }
+DTFecha Viaje::getFecha(){ return fecha; }
+std::string Viaje::getOrigen(){ return origen; }
+std::string Viaje::getDestino(){ return destino; }
+int Viaje::getAsientosPublicados(){ return asientosPublicados; }
+float Viaje::getPrecio(){ return precio; }
+
+void Viaje::setCodigo(int codigo){ this->codigo = codigo; }
+void Viaje::setFecha(DTFecha fecha){ this->fecha = fecha; }
+void Viaje::setOrigen(std::string origen){ this->origen = origen; }
+void Viaje::setDestino(std::string destino){ this->destino = destino; }
+void Viaje::setAsientosPublicados(int asientosPublicados){ this->asientosPublicados = asientosPublicados; }
+void Viaje::setPrecio(int precio){ this->precio = precio; }
+
+std::string Viaje::getConductor(){
+
+}
+bool Viaje::esBuscado(DTFecha fecha, std::string origen, std::string destino, int asientosPublicados){
+
+}
+int Viaje::cantAsientosValida(int asientosRes, int asientos, int asientosPublicados){
+
+}
+DTListarViaje Viaje::getDTListarViaje(){
+
+}
+int Viaje::cantAsientosRes(){
+
+}
+DTConsultaViaje Viaje::getDataViaje(){
+
+}
+void Viaje::agregarReserva(Reserva r){
+
+}
+
+/*FALTAN FUNCIONES*/
