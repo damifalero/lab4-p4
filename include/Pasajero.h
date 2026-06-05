ecc5ef9bb0a00c3ae1844d637f194d9a90109978
Damian Falero <damianfalero97@gmail.com>
2026-06-04 22:40:17 -0300

    Pasajero

--- include/Pasajero.h
+++ include/Pasajero.h
@@ -2,15 +2,21 @@
 #define PASAJERO_H
 
 #include "Usuario.h"
+#include "Reserva.h"
+
+#include <set>
 #include <string>
 
 class Pasajero : public Usuario {
 private:
     std::string ci;
+    std::set<Reserva*> reservas;
 
 public:
     Pasajero(std::string nickname, std::string nombre, std::string contrasena, std::string email, std::string documento);
     ~Pasajero();
+    std::string getci();
+    std::set<Reserva*> getReservas();
 };
 
 #endif
