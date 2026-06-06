3fbefab9e7d4d3414c323f1d3b435356ea4773bd
Matheo Perdomo <matheoperdomo07@gmail.com>
2026-06-05 22:43:25 -0300

    Conductor, Vehiculo y Fabrica

--- include/Vehiculo.h
+++ include/Vehiculo.h
@@ -1,9 +1,15 @@
 #ifndef VEHICULO_H
 #define VEHICULO_H
 
-#include "TipoVehiculo.h"
+#include <iostream>
 #include <string>
+#include <set>
+#include "DTVehiculosConductor.h"
+#include "TipoVehiculo.h"
+#include "DTListarViaje.h"
+#include "Usuario.h"
 
+class Viaje;
 class Vehiculo {
 private:
     std::string matricula;
@@ -11,10 +17,32 @@ private:
     std::string marca;
     std::string modelo;
     TipoVehiculo tipo;
+    Usuario* usuario;
+    std::set<Viaje*> viajes;
 
 public:
     Vehiculo(std::string matricula, int capacidad, std::string marca, std::string modelo, TipoVehiculo tipo);
     ~Vehiculo();
+    //getters
+    std::string getMatricula();
+    int getCapacidad();
+    std::string getMarca();
+    std::string getModelo();
+    TipoVehiculo getTipo();
+    Usuario getConductor();
+    std::set<DTListarViaje> getSetDTListarViaje();
+    DTVehiculosConductor getDTVehiculosConductor();
+    //setters
+    void setMatricula(std::string matricula);
+    void setCapacidad(int capacidad);
+    void setMarca(std::string marca);
+    void setModelo(std::string modelo);
+    void setTipo(TipoVehiculo tipo);
+    void setUsuario(Usuario* usuario);
+
+    //metodos
+    bool hayViajesConductor();
+    void asociarViaje(Viaje* viaje);
 };
 
 #endif
