b3a9ae0bed06ae6eb2ba5d46613929ebf5d1eabc
Damian Falero <damianfalero97@gmail.com>
2026-06-06 22:34:02 -0300

    GestionViajeController.h

---
+++ include/GestionViajeController.h
@@ -0,0 +1,43 @@
+#ifndef GESTION_VIAJE_CONTROLLER_H
+#define GESTION_VIAJE_CONTROLLER_H
+
+#include <set>
+#include <string>
+#include "IGestionViajeController.h"
+
+class GestionViajeController : public IGestionViajeController {
+private:
+    static GestionViajeController* instancia;
+
+    int ultimoViaje;
+    int codigoRecordado;
+
+    GestionViajeController();
+
+public:
+    static GestionViajeController* getInstance();
+
+    virtual ~GestionViajeController();
+
+    std::set<std::string> listarPasajeros();
+
+    std::set<DTConsultaViaje> consultarViaje(DTFecha fecha, std::string origen, std::string destino, int asientos);
+
+    bool altaViaje(std::string matricula, DTFecha fecha, std::string origen, std::string destino, int asientos, float precio);
+
+    DTDetalleViaje detalleViaje(int codigo);
+
+    std::set<DTUsuarioViaje> listarUsuariosViaje();
+
+    std::set<DTListarViaje> listarViajes(std::string nick);
+
+    std::set<DTListarViaje> listarViaje();
+
+    void eliminarViaje();
+
+    void cancelarEliminarViaje();
+
+    bool generarReserva(std::string nickname, int codigo, int asientos);
+};
+
+#endif
