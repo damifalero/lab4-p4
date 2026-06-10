30cc190797d3cd0c6e0bfac1402ba36d0a369787
matheoperdomo <matheoperdomo07@gmail.com>
2026-06-10 19:09:56 -0300

    UsuarioController.h

---
+++ include/UsuarioController.h
@@ -0,0 +1,25 @@
+#ifndef USUARIO_CONTROLLER_H
+#define USUARIO_CONTROLLER_H
+
+#include <string>
+#include <set>
+#include "DTUsuario.h"
+#include "DTVehiculosConductor.h"
+#include "TipoLibreta.h"
+#include "TipoVehiculo.h"
+
+class UsuarioController {
+    private:
+        std::string nicknameRecordado;
+        int codigoRecordado;
+    public:
+        UsuarioController();
+        bool altaPasajero(std::string nickname, std::string nombre, std::string contrasena, std::string email, std::string ci);
+        bool altaConductor(std::string nickname, std::string nombre, std::string contrasena, std::string email, std::set<TipoLibreta> libretas);
+        std::set<DTUsuario> listarUsuarios();
+        bool calificarUsuario(std::string nicknameCalificado, int calificacion);
+        int registrarVehiculo(std::string nickname, std::string matricula, int capacidad, std::string marca, std::string modelo, TipoVehiculo tipo);
+        std::set<DTVehiculosConductor> listarVehiculosConductor(std::string nickname);
+};
+
+#endif // USUARIO_CONTROLLER_H
